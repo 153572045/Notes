0138ecde9e29c029ed77fb0ac9e26dde7fd29b6d
chenxiaoxian7 <chenxiaoxian7@jd.com>
2022-04-11 22:59:02 +0800

    abc

--- src/main/java/cxx/note/Test.java
+++ src/main/java/cxx/note/Test.java
@@ -2,18 +2,71 @@ package cxx.note;
 
 import cxx.note.algorithm.sort.QuickSort;
 import cxx.note.algorithm.sort.SortAge;
+import redis.clients.jedis.Jedis;
 
-import java.util.Arrays;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.*;
 
 public class Test {
 
     public static final String b = "abc";
 
     public static void main(String[] args) throws Exception{
-        System.out.println(Math.pow(2,4));
+        HashMap<String, String> map = new HashMap<>(16);
+        Class clazz = map.getClass();
+        Field threshod = clazz.getDeclaredField("threshold");
+        threshod.setAccessible(true);
+        Method capaticy = clazz.getDeclaredMethod("capacity");
+        capaticy.setAccessible(true);
+        System.out.println("初始容量：" + capaticy.invoke(map) + "初始阈值：" + threshod.get(map) + "元素数量：" + map.size());
+        map.put("abc", "abc");
+        System.out.println("初始容量：" + capaticy.invoke(map) + "初始阈值：" + threshod.get(map) + "元素数量：" + map.size());
 
     }
 
+}
+
+class Solution {
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return null;
+        }
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] a = new int[rows * cols];
+        col(matrix, a, cols, rows, 0, 0);
+        return a;
+    }
+
+    private void col(int[][] matrix, int[] result, int cols, int rows, int tag, int index) {
+        if(cols <= 0 || rows <= 0) {
+            return ;
+        }
+        boolean ju = false;
+        int temp = tag;
+        for(int i = 0; i < cols; ++i) {
+            result[index++] = matrix[tag][temp++];
+        }
+        temp--;
+        int temp1 = tag + 1;
+        for(int i = 0; i < rows - 1; ++i) {
+            result[index++] = matrix[temp1++][temp];
+            ju = true;
+        }
+        temp1 = temp1 - 1;
+        for(int i = 0; i < cols - 1; ++i) {
+            if(ju == false) {
+                break;
+            }
+            result[index++] = matrix[temp1][--temp];
+        }
+        for(int i = 0; i < rows - 2; ++i) {
+            result[index++] = matrix[--temp1][temp];
+        }
+        col(matrix, result, cols - 2, rows - 2, tag + 1, index);
+
+    }
 }
 
 class FTEST {
--- src/main/java/cxx/note/read.md
+++ src/main/java/cxx/note/read.md
@@ -21,5 +21,5 @@
 
 
 二、知识点地址
-https://blog.csdn.net/v123411739/article/details/78996181   hashmap学习25
+https://blog.csdn.net/v123411739/article/details/78996181   map学习25
 
