17c60500215e6403813da3fe274a01f10357bbed
chenxiaoxian7 <chenxiaoxian7@jd.com>
2022-08-09 18:29:40 +0800

    a

--- src/main/java/cxx/code/Temp.java
+++ src/main/java/cxx/code/Temp.java
@@ -10,38 +10,15 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Temp {
 
 
-    public static void main(String[] args) {
-        int[][] exm = new int[4][4];
-        exm[0] = new int[] {0, 1, 1, 0};
-        exm[1] = new int[] {0, 1, 1, 0};
-        exm[2] = new int[] {0, 0, 0, 0};
-        exm[3] = new int[] {0, 1, 1, 0};
-        System.out.print(max(exm));
+    public static void main(String[] args) throws Exception {
+        ReentrantLock lock = new ReentrantLock();
+        lock.lock();
+        Condition condition = lock.newCondition();
+        condition.await();
+        condition.signal();
+
+        lock.unlock();
     }
-
-    static int max(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int max = 0;
-        int[][] area = new int[rows][cols];
-        for(int i = 0; i < rows; ++i) {
-            for(int j = 0; j <cols; ++j) {
-                if(matrix[i][j] == 1) {
-                    if(i == 0 || j == 0) {
-                        area[i][j] = 1;
-                    } else {
-                        area[i][j] = 1 + Math.min(Math.min(matrix[i - 1][j],matrix[i][j -1]),matrix[i - 1][j - 1]);
-                    }
-                    if(area[i][j] > max) {
-                        max = area[i][j];
-                    }
-                }
-            }
-        }
-        return max * max;
-    }
-
-
 
 
 }
